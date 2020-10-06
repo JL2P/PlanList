a22a93d2183273ddb8b6ee485ca0b41f9a271ac3
shoon2430 <shoon2430@gmail.com>
2020-10-07 08:40:09 +0900

    라이브러리 설치

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+//추가
+//시멘틱 UI 적용
+import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
