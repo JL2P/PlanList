e54d95d302c7273636f8f8124da45023f1ea192f
shoon2430 <shoon2430@gmail.com>
2020-10-07 17:54:15 +0900

    헤더 컴포넌트 분리 및 메인페이지 Form 추가

---
+++ src/Planlist/Main/View/MainFormDesktopView.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Container, Header, Input } from "semantic-ui-react";
+
+const MainFormDesktopView = ({ Media }) => {
+  return (
+    <div style={{ background: "#1b1c1d" }}>
+      <Media greaterThan="mobile">
+        <Container
+          text
+          textAlign="center"
+          style={{ paddingTop: "1em", paddingBottom: "3.5em" }}
+        >
+          <Header
+            as="h1"
+            content="The Plan List."
+            style={{
+              fontSize: "4em",
+              fontWeight: "normal",
+              color: "white",
+            }}
+          />
+          <Header
+            as="h5"
+            content="Put your plan into action."
+            style={{
+              fontSize: "1.5em",
+              fontWeight: "normal",
+              color: "white",
+            }}
+          />
+          <Input fluid icon="write" placeholder="Write what to do today" />
+        </Container>
+      </Media>
+    </div>
+  );
+};
+
+export default MainFormDesktopView;
--- src/Planlist/PlanlistRoutes.js
+++ src/Planlist/PlanlistRoutes.js
@@ -1,9 +1,10 @@
 export { default as Header } from "./Header/HeaderPage";
 export { default as Main } from "./Main/MainPage";
+
 export { default as SignIn } from "./Sign/Signin/SigninPage";
 export { default as SignUp } from "./Sign/Singup/SignupPage";
 //todo 추가
-export { default as Todo } from  "./todo/TodoPage";
+export { default as Todo } from "./todo/TodoPage";
 
 // Account는 Account 내에서도 Route가 있음
 export { default as Account } from "./Account/AccountRoutes";
