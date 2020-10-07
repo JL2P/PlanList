5227260cf209cfd7177dc68678299d1dab8559c5
2myungho <dl_audgh@naver.com>
2020-10-07 15:59:57 +0900

    로그인 회원가입 view container 컴포넌트 add

---
+++ src/Planlist/Sign/Signin/view/SigninView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SigninView = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SigninView;
--- src/Planlist/Sign/Singup/SignupPage.jsx
+++ src/Planlist/Sign/Singup/SignupPage.jsx
@@ -1,17 +1,8 @@
 import React from "react";
-import { Container, Header } from "semantic-ui-react";
+import SignupContainer from "./container/SignupContainer";
 
 const SignupPage = () => {
-  return (
-    <Container text style={{ marginTop: "7em" }}>
-      <Header as="h1">This is SigninPage</Header>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>
-        A text container is used for the main container, which is useful for
-        single column layouts.
-      </p>
-    </Container>
-  );
+  return <SignupContainer />
 };
 
 export default SignupPage;
