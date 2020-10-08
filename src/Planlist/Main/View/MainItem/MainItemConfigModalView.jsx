9dc9a23e3e33da5a402e3bb98527e5efd0bc64e7
shoon2430 <shoon2430@gmail.com>
2020-10-08 14:04:21 +0900

    Todo 수정 및 추천을 위한 모달 추가

--- src/Planlist/Main/View/MainItem/MainItemConfigModalView.jsx
+++ src/Planlist/Main/View/MainItem/MainItemConfigModalView.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Header, Icon, Modal, Segment } from "semantic-ui-react";
+import { Button, Header, Icon, Modal } from "semantic-ui-react";
 
 const MainItemConfigModalView = ({ open, onModal }) => {
   return (
