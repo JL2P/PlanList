9b322427a894403cf720394913041c3c94f9357e
shoon2430 <shoon2430@gmail.com>
2020-10-08 11:20:15 +0900

    헤더 데스크탑 버전과 모바일 버전에 맞게 나눔

--- src/Planlist/Header/container/HeaderMainContainer.jsx
+++ src/Planlist/Header/container/HeaderMainContainer.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
-import HeaderMainView from "../view/HeaderMainView";
-class HeaderContainer extends Component {
+import { createMedia } from "@artsy/fresnel";
+import HeaderDesktopView from "../view/HeaderDesktopView";
+import HeaderMobileView from "../view/HeaderMobileView";
+
+class HeaderMainContainer extends Component {
   render() {
-    return <HeaderMainView />;
+    const { MediaContextProvider, Media } = createMedia({
+      breakpoints: {
+        mobile: 0,
+        tablet: 768,
+        computer: 1024,
+      },
+    });
+
+    return (
+      <MediaContextProvider>
+        <HeaderDesktopView Media={Media} />
+        <HeaderMobileView Media={Media} />
+      </MediaContextProvider>
+    );
   }
 }
 
-export default HeaderContainer;
+export default HeaderMainContainer;
---
+++ src/Planlist/Header/view/HeaderMobileView.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  Menu,
+  Segment,
+  Sidebar,
+  Icon,
+} from "semantic-ui-react";
+
+const HeaderMobileView = ({ Media }) => {
+  const [sidebarOpened, setSidebarOpened] = useState(false);
+
+  const handleSidebarHide = () => setSidebarOpened(false);
+  const handleToggle = () => setSidebarOpened(true);
+
+  return (
+    <Media as={Sidebar.Pushable} at="mobile">
+      <Sidebar.Pushable>
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          inverted
+          onHide={handleSidebarHide}
+          vertical
+          visible={sidebarOpened}
+        >
+          <Menu.Item as="a" active>
+            Home
+          </Menu.Item>
+          <Menu.Item as="a">Work</Menu.Item>
+          <Menu.Item as="a">Company</Menu.Item>
+          <Menu.Item as="a">Careers</Menu.Item>
+          <Menu.Item as="a">Log in</Menu.Item>
+          <Menu.Item as="a">Sign Up</Menu.Item>
+        </Sidebar>
+
+        <Sidebar.Pusher dimmed={sidebarOpened}>
+          <Segment
+            inverted
+            textAlign="center"
+            style={{ minHeight: 80, padding: "1em 0em" }}
+            vertical
+          >
+            <Container>
+              <Menu inverted pointing secondary size="large">
+                <Menu.Item onClick={handleToggle}>
+                  <Icon name="sidebar" />
+                </Menu.Item>
+                <Menu.Item position="right">
+                  <Button as="a" inverted>
+                    Log in
+                  </Button>
+                  <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
+                    Sign Up
+                  </Button>
+                </Menu.Item>
+              </Menu>
+            </Container>
+          </Segment>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </Media>
+  );
+};
+
+export default HeaderMobileView;
