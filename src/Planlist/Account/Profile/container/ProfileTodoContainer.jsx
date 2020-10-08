620140403acd67051a49fbafc34d2601618b215a
song <eunsonglee0204@gmail.com>
2020-10-08 09:09:46 +0900

    Account페이지, profile, todo 화면 구성 수정

--- src/Planlist/Account/Profile/container/ProfileTodoContainer.jsx
+++ src/Planlist/Account/Profile/container/ProfileTodoContainer.jsx
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import ProfileTodoView from "../view/ProfileTodoView";
 
 class ProfileTodoContainer extends Component {
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
   render() {
     //기능들구현해서 prop로 넘겨주는 작업
 
-    return <ProfileTodoView />;
+    return <ProfileTodoView handleItemClick={this.handleItemClick} />;
   }
 }
 
