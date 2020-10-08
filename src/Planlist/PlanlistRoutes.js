049ae580b77b53fe8cc362d7427c2d94e9a9f599
2myungho <dl_audgh@naver.com>
2020-10-08 14:45:58 +0900

    signin signup View add

--- src/Planlist/PlanlistRoutes.js
+++ src/Planlist/PlanlistRoutes.js
@@ -1,7 +1,8 @@
 export { default as Header } from "./Header/HeaderPage";
 export { default as Main } from "./Main/MainPage";
 
-export { default as SignIn } from "./Sign/SignPage";
+export { default as SignIn } from "./Sign/Signin/SigninPage";
+export { default as SignUp } from "./Sign/Signup/SignupPage";
 
 //todo 추가
 export { default as Todo } from "./todo/TodoPage";
