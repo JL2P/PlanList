09d48616de7408a2d189bd669bfcc5932e104f2f
song <eunsonglee0204@gmail.com>
2020-10-12 23:26:18 +0900

    모달 창에 modify 메뉴 추가

--- src/Planlist/Account/Profile/view/ProfileManageItem/ProfileSettingModalView.jsx
+++ src/Planlist/Account/Profile/view/ProfileManageItem/ProfileSettingModalView.jsx
@@ -1,28 +1,55 @@
 import React from "react";
-import { Container, Modal, Segment, Menu, Grid } from "semantic-ui-react";
+import {
+  Container,
+  Modal,
+  Segment,
+  Menu,
+  Grid,
+  Button,
+  Icon,
+} from "semantic-ui-react";
+import ProfileModifyView from "../../../ProfileModify/view/ProfileModifyView";
+import ProfileManageView from "../ProfileManageView";
+import "../../../ProfileModify/view/ProfileModifyView.scss";
 
-const ProfileSettingModalView = ({ open, onOpen, activeItem }) => {
-  const handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+const ProfileSettingModalView = ({
+  open,
+  onOpen,
+  activeItem,
+  handleItemClick,
+  account,
+}) => {
+  // const handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   return (
-    <Modal open={open} size={"mini"}>
+    <Modal open={open} height={10}>
       <Modal.Header>
         <Container textAlign="center">설정</Container>
       </Modal.Header>
-      <Modal.Content scrolling>
-        {/* <Grid>
-          <Grid.Column width={4}>
-            <Menu fluid vertical tabular>
+      <Modal.Content scrolling image>
+        <Grid>
+          <Grid.Column width={3}>
+            <Menu vertical tabular pointing secondary>
               <Menu.Item
-                name="bio"
-                active={activeItem === "bio"}
-                onClick={handleItemClick}
+                name="프로필 수정"
+                active={activeItem === "프로필 수정"}
+                // onClick={handleItemClick}
+                onClick={() => handleItemClick("프로필 수정")}
               />
               <Menu.Item
-                name="pics"
-                active={activeItem === "pics"}
-                onClick={handleItemClick}
+                name="비밀번호 변경"
+                active={activeItem === "비밀번호 변경"}
+                // onClick={handleItemClick}
+                onClick={() => handleItemClick("비밀번호 변경")}
               />
               <Menu.Item
+                name="공개 범위 설정"
+                active={activeItem === "공개 범위 설정"}
+                // onClick={handleItemClick}
+                onClick={() => handleItemClick("공개 범위 설정")}
+              />
+
+              {/* <Menu.Item
                 name="companies"
                 active={activeItem === "companies"}
                 onClick={handleItemClick}
@@ -31,26 +58,45 @@ const ProfileSettingModalView = ({ open, onOpen, activeItem }) => {
                 name="links"
                 active={activeItem === "links"}
                 onClick={handleItemClick}
-              />
+              /> */}
             </Menu>
           </Grid.Column>
 
-          <Grid.Column stretched width={7}>
-            <Segment>
-              This is an stretched grid column. This segment will always match
-              the tab height
-            </Segment>
+          <Grid.Column stretched width={12}>
+            <div>
+              {activeItem === "프로필 수정" && (
+                <ProfileModifyView account={account} />
+              )}
+              {activeItem === "비밀번호 변경" && (
+                <ProfileModifyView account={account} />
+              )}
+              {activeItem === "공개 범위 설정" && (
+                <p>
+                  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;공개
+                  범위 설정
+                </p>
+              )}
+            </div>
           </Grid.Column>
-        </Grid> */}
-        <Modal.Description>
+        </Grid>
+
+        {/* <Modal.Description>
           <Segment textAlign="center">프로필 수정</Segment>
           <Segment textAlign="center">비밀번호 변경</Segment>
           <Segment textAlign="center">할 일 공개 범위 설정</Segment>
           <Segment textAlign="center" onClick={() => onOpen(false)} primary>
             취소
           </Segment>
-        </Modal.Description>
+        </Modal.Description> */}
       </Modal.Content>
+      <Modal.Actions>
+        <Button basic color="red" onClick={() => onOpen(false)}>
+          <Icon name="remove" /> No
+        </Button>
+        <Button color="green" onClick={() => onOpen(false)}>
+          <Icon name="checkmark" /> Yes
+        </Button>
+      </Modal.Actions>
     </Modal>
   );
 };
--- src/Planlist/Account/ProfileModify/view/ProfileModifyView.jsx
+++ src/Planlist/Account/ProfileModify/view/ProfileModifyView.jsx
@@ -1,22 +1,116 @@
 import React, { Component } from "react";
-import { Card, Segment } from "semantic-ui-react";
+import { Card, Form, Grid, Input, Segment } from "semantic-ui-react";
+import "./ProfileModifyView.scss";
 
-const ProfileModifyView = ({account}) => {
+const ProfileModifyView = ({ account }) => {
+  /**
+	private String accountId; 
+  private String name;  
+  private String email; 
+	private String password; //
+	private String birth; 
+	private String gender; 
+	private String address; 
+	private String phone; 
+	private String introduce; 
+	private int rating; //
+	private String usedAt; //
+	private String displayAt; //
+ */
+  const colWidth = 4;
+  return (
+    <Form>
+      <Grid stackable>
+        <Grid.Row columns={2}>
+          <Grid.Column width={colWidth}>
+            <aside>
+              <label>아이디</label>
+            </aside>
+          </Grid.Column>
+          <Grid.Column>
+            <Input fluid placeholder={account.accountId} />
+          </Grid.Column>
+        </Grid.Row>
 
-    return (
-    <div>
-      <Card>
+        <Grid.Row columns={2}>
+          <Grid.Column width={colWidth}>
+            <aside>
+              <label>이름</label>
+            </aside>
+          </Grid.Column>
+          <Grid.Column>
+            <Input fluid placeholder={account.name} />
+          </Grid.Column>
+        </Grid.Row>
 
-      <Segment.Group compact>
-        
-        <Segment textAlign="center">{account.email}</Segment>
-        <Segment textAlign="center">프로필 수정</Segment>
-        <Segment textAlign="center">프로필 수정</Segment>
-        <Segment textAlign="center">프로필 수정</Segment>
-      </Segment.Group>
-      </Card>
-    </div>
-    );
-}
+        <Grid.Row columns={2}>
+          <Grid.Column width={colWidth}>
+            <aside>
+              <label>이메일</label>
+            </aside>
+          </Grid.Column>
+          <Grid.Column>
+            <Input fluid placeholder={account.email} />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns={2}>
+          <Grid.Column width={colWidth}>
+            <aside>
+              <label>전화번호</label>
+            </aside>
+          </Grid.Column>
+          <Grid.Column>
+            <Input fluid placeholder={account.phone} />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns={2}>
+          <Grid.Column width={colWidth}>
+            <aside>
+              <label>생년월일</label>
+            </aside>
+          </Grid.Column>
+          <Grid.Column>
+            <Input fluid placeholder={account.birth} />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns={2}>
+          <Grid.Column width={colWidth}>
+            <aside>
+              <label>성별</label>
+            </aside>
+          </Grid.Column>
+          <Grid.Column>
+            <Input fluid placeholder={account.gender} />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns={2}>
+          <Grid.Column width={colWidth}>
+            <aside>
+              <label>주소</label>
+            </aside>
+          </Grid.Column>
+          <Grid.Column>
+            <Input fluid placeholder={account.address} />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns={2}>
+          <Grid.Column width={colWidth}>
+            <aside>
+              <label>소개글</label>
+            </aside>
+          </Grid.Column>
+          <Grid.Column>
+            <Input fluid placeholder={account.introduce} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Form>
+  );
+};
 
 export default ProfileModifyView;
