ee3b441295eba442f6d27a852c9f55c45f7b8d98
song <eunsonglee0204@gmail.com>
2020-10-15 17:25:23 +0900

    profileManage js파일 jsx로 변경

--- src/Planlist/Account/Profile/container/ProfileMangeContainer.jsx
+++ src/Planlist/Account/Profile/container/ProfileMangeContainer.jsx
@@ -2,19 +2,17 @@ import React, { Component } from "react";
 import ProfileManageView from "../view/ProfileManageView";
 import { inject, observer } from "mobx-react";
 
-
 @inject("Store")
 @observer
 class ProfileMangeContainer extends Component {
-  
   componentDidMount() {
-    console.log("componentDidMount")
+    console.log("componentDidMount");
     const { account } = this.props.Store;
     account.selectUser("song");
   }
 
   onModifyAccount = () => {
-    const {account} = this.props.Store;
+    const { account } = this.props.Store;
     account.userModify(account);
   };
   render() {
@@ -22,10 +20,15 @@ class ProfileMangeContainer extends Component {
     // Store에서 account Store가져오기
     console.log("render");
     const { account } = this.props.Store;
-    console.log("??",account);
+    console.log("??", account);
     // console.log(account.accountDetail);
 
-    return <ProfileManageView account={account.getAccount} onModifyAccount={this.onModifyAccount} />;
+    return (
+      <ProfileManageView
+        account={account.getAccount}
+        onModifyAccount={this.onModifyAccount}
+      />
+    );
   }
 }
 
