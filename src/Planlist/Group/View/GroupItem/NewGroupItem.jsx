ba499c040b9fc19df576cdcd81cf59ce8d36551d
2myungho <dl_audgh@naver.com>
2020-10-22 01:04:24 +0900

    group page에 newGroup 추가

--- src/Planlist/Group/View/GroupItem/NewGroupItem.jsx
+++ src/Planlist/Group/View/GroupItem/NewGroupItem.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { Card ,Image } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import { Card ,Image,Icon } from "semantic-ui-react";
 import '../../GroupStyle/Group.scss';
 
 const NewGroupItem = () => {
   return (
-    <Link to="/">
-      <Card className="group_card" raised>
-        <Card.Content>
-          <Card.Header className="group_Card_header">그룹 만들기</Card.Header>
-        </Card.Content>
-      </Card>
-    </Link>
+    <Card className="group_card, Newgroup_card" raised>
+        <Icon name='add circle' className="Newgroup_circle" />
+        <Card.Header className="group_Card_header">그룹 만들기</Card.Header>
+    </Card>
   );
 };
 
