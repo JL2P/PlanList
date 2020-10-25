72adfac69c068217f550b559146870f77cac0a76
2myungho <dl_audgh@naver.com>
2020-10-25 23:45:24 +0900

    groupDetail list modal 버튼 추가

--- src/Planlist/Group/GroupDetail/View/DetailGroupCenter/DetailGroupAllListModal.jsx
+++ src/Planlist/Group/GroupDetail/View/DetailGroupCenter/DetailGroupAllListModal.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { Button, Header, Image, Modal } from "semantic-ui-react";
 
-const DetailGroupAllListModal = ({setOpen,item}) => {
+const DetailGroupAllListModal = ({ setOpen, item }) => {
   return (
     <>
       <Modal.Header>Select a Photo</Modal.Header>
       <Modal.Content image>
         <Image size="medium" src={item.imgUrl} wrapped />
         <Modal.Description>
-        <Header>{item.title}</Header>
-        <p>{item.description}</p>
-        <p>참여한 멤버 수 : {item.rating}명</p>
+          <Header>{item.title}</Header>
+          <p>{item.description}</p>
+          <p>참여한 멤버 수 : {item.rating}명</p>
         </Modal.Description>
       </Modal.Content>
+      <Modal.Actions>
+        <Button color="black" onClick={() => setOpen(false)}>
+          수정
+        </Button>
+        <Button color="red" onClick={() => setOpen(false)}>
+          삭제
+        </Button>
+      </Modal.Actions>
     </>
   );
 };
