99b1ff763cdb4552ba6943f9ab58f293d640d61d
2myungho <dl_audgh@naver.com>
2020-10-27 18:08:43 +0900

    좌측 nav에 카테고리 리스트별로 출력

--- src/Planlist/Group/GroupCategory/View/GroupCategoryNav.jsx
+++ src/Planlist/Group/GroupCategory/View/GroupCategoryNav.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Menu, Input } from 'semantic-ui-react'
 
-const GroupCategoryNav = () => {
-    return (
-        <div>
-            gdgdgd
-        </div>
-    );
+const GroupCategoryNav = ({
+        sampleData,
+        onCategoryList_select
+    }) => {
+
+    const [activeItem, setActiveItem] = useState("전체 그룹");
+    const handleItemClick = (e, { name }) => setActiveItem(name);
+
+    const group_category_nav_padding = {padding:"1.5rem"}
+
+    const GroupCategoryNavList = sampleData.map((item,index) => (
+        <Menu.Item
+            style={group_category_nav_padding}
+            name={item.text}
+            key={index}
+            active={activeItem === item.text}
+            onClick={handleItemClick}
+            onMouseUp={() => onCategoryList_select(item)}
+        />
+    ))
+  return (
+    <div>
+      <Menu pointing secondary vertical style={{width:"100%"}}>
+        <Menu.Item style={group_category_nav_padding}>
+            <h2>카테고리</h2>
+            <Input icon='search' placeholder='그룹 검색' />
+        </Menu.Item>
+        {GroupCategoryNavList}
+      </Menu>
+    </div>
+  );
 };
 
 export default GroupCategoryNav;
