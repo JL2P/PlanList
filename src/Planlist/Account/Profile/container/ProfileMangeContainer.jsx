7fb55c282194b0113395fb2cb98608ed5df9d1ab
song <eunsonglee0204@gmail.com>
2020-10-27 18:14:25 +0900

    날짜 받아오기

--- src/Planlist/Account/Profile/container/ProfileMangeContainer.jsx
+++ src/Planlist/Account/Profile/container/ProfileMangeContainer.jsx
@@ -22,13 +22,10 @@ class ProfileMangeContainer extends Component {
     console.log("render");
     const { account, todo } = this.props.Store;
     const todos = todo.getTodos2;
+    const today = todo.getToday;
     console.log("??", account);
-
-    const today_date = new Date(); // 현재 날짜 받아오기
-    const year = today_date.getFullYear();
-    const month = today_date.getMonth() + 1; // 1월:0
-    const date = today_date.getDate(); // 날짜
-    const today = year + "-" + month + "-" + date;
+    console.log(today);
+    
     const count = todos.filter((item) => item.end_time >= today).length;
 
     // console.log(account.accountDetail);
@@ -37,7 +34,7 @@ class ProfileMangeContainer extends Component {
         <ProfileManageView
           account={account.getAccount2}
           onModifyAccount={this.onModifyAccount}
-          todo_count = {count}
+          todo_count={count}
         />
       </div>
     );
