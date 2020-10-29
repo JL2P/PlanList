bd69f994d0e46d08ecbd3d3bdffacab211952d53
2myungho <dl_audgh@naver.com>
2020-10-30 02:01:05 +0900

    실데이터 연결을 위한 GroupModel 파일 생성

---
+++ src/Planlist/Api/model/group/GroupAddModel.js
@@ -0,0 +1,9 @@
+export default class GroupAddModel{
+    constructor(groupObj){
+        this.imgUrl = groupObj.imgUrl; //이미지 URL
+        this.title = groupObj.title //제목
+        this.description = groupObj.description //설명글
+        this.category = groupObj.category; //카테고리
+        this.master = groupObj.master; //그룹장
+    }
+}
