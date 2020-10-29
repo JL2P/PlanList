5189e6c73fa169d766efb6a45ca519c2f93abc1b
2myungho <dl_audgh@naver.com>
2020-10-30 01:58:58 +0900

    그룹 전체 리스트 조회, 그룹 디테일 조회 등 구현

--- src/Planlist/Group/GroupMenu/Container/GroupContainer.jsx
+++ src/Planlist/Group/GroupMenu/Container/GroupContainer.jsx
@@ -10,30 +10,57 @@ import { inject, observer } from "mobx-react";
 @observer
 class GroupContainer extends Component {
 
+    //그룹 생성
     onCreateGroup = (e, createObj) => {
         e.preventDefault();
         const { group } = this.props.Store;
         group.createGroup(createObj);
     }
 
+    //그룹 전체 리스트 조회
+    onAllGroups = () => {
+        const { group } = this.props.Store;
+        group.getApiGroups();
+    }
+    //그룹 디테일 조회
+    onGroupDetail_page = (groupId) => {
+        const { group } = this.props.Store;
+        group.groupDetail_page(groupId);
+    }
+    //카테고리 사진 클릭시 getSelect_Group_categoryList 변경
+    onCategorySelect = (category) => {
+        const { group } = this.props.Store;
+        group.select_Group_categoryList = category
+    }
+
     render() {
         const { group } = this.props.Store;
         const my_sampleData = group.getMyTodo;
         const best_sampleData = group.getBestTodo;
-        const category_sampleData = group.getCategoryTodo;
-        const recommend_sampleData= group.getRecommendTodo;
-        const categoryList = group.getCategoryList;
+
+        const {
+            getGroups,
+            getDetailGroup_open,
+            getCategoryList,
+        } = group
         
         return (
             <div>
                 <MyGroupView 
                     sampleData={my_sampleData} 
-                    categoryList={categoryList}
+                    categoryList={getCategoryList}
                     onCreateGroup={this.onCreateGroup}
                 />
                 <BestGroupView sampleData={best_sampleData} />
-                <CategoryGroupView sampleData={category_sampleData} />
-                <RecommendGroupView sampleData={recommend_sampleData} />
+                <CategoryGroupView 
+                    categoryList={getCategoryList} 
+                    onCategorySelect={this.onCategorySelect}
+                />
+                <RecommendGroupView
+                    groups={getGroups} 
+                    onAllGroups={this.onAllGroups}
+                    onGroupDetail_page={this.onGroupDetail_page}
+                />
             </div>
         );
     }
