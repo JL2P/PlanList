b4c18f919d4063009a7788863bf9ac95e907f3ad
2myungho <dl_audgh@naver.com>
2020-10-31 01:56:41 +0900

    GroupModifyModel.js에 id 추가

---
+++ src/Planlist/Api/model/group/GroupModifyModel.js
@@ -0,0 +1,7 @@
+export default class GroupAddModel{
+    constructor(groupObj){
+        this.id = groupObj.id;
+        this.title = groupObj.title //제목
+        this.description = groupObj.description //설명글
+    }
+}
