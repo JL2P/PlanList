f5922398cf0f03c84ffc1b3783a9b14a6e9559d2
shoon2430 <shoon2430@gmail.com>
2020-10-31 21:30:55 +0900

    백엔드 댓글 대댓글 데이터와  매핑되는 model추가

--- src/Planlist/Api/model/comment/CommentModels.js
+++ src/Planlist/Api/model/comment/CommentModels.js
@@ -1,21 +1,30 @@
+import { SubCommentModel } from './SubCommentModels';
+
 // 댓글 조회시 서버의 DTO와 매핑되는 모델
 export class CommentModel{
     constructor(commentObj){
         this.commentId = commentObj.commentId;
+        this.todoId = commentObj.todoId;
         this.text = commentObj.text;
         this.writer = commentObj.writer;
         this.likePoint = commentObj.likePoint;
-        this.upper = commentObj.upper;
+        this.subComments = commentObj.subComments.map(subComment => new SubCommentModel(subComment));
     }
 };
 
 // 댓글 생성시 서버의 DTO와 매핑되는 모델
 export class CommentAddModel{
     constructor(commentObj){
-        this.title = commentObj.title;
-        this.description = commentObj.description;
-        this.category = commentObj.category;
-        this.writer = commentObj.writer;
-        this.upper = commentObj.upper;
+        this.text = commentObj.text;    // 제목
+        this.writer = commentObj.writer;  // 작성자ID
+    }
+};
+
+
+// 댓글 수정시 서버의 DTO와 매핑되는 모델
+export class CommentModifyModel{
+    constructor(commentObj){
+        this.commentId = commentObj.commentId;    // 제목
+        this.text = commentObj.text;  // 내용
     }
 };
