a94b36a99c919fa79e5663fae0865208e58407f4
2myungho <dl_audgh@naver.com>
2020-11-05 18:50:35 +0900

    groupDetail profile 부분 멤버수 나타내기

--- src/Planlist/Group/GroupDetail/Container/DetailProfileContainer.jsx
+++ src/Planlist/Group/GroupDetail/Container/DetailProfileContainer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import GroupProfile from '../View/GroupProfileView';
+import GroupProfileView from '../View/GroupProfileView';
 import { inject, observer } from "mobx-react";
 
 @inject("Store")
@@ -8,19 +8,22 @@ class DetailProfileContainer extends Component {
     render() {
         const { group } = this.props.Store;
         const { account } = this.props.Store;
-        const my_sampleData = group.getMyTodo;
 
         const {
             getDetailGroup_open,
+            detailGroup_memberLength,
+            getGroups
         } = group
 
         const {loginAccount} = account;
+
         return (
             <div>
-                <GroupProfile 
-                    sampleData={my_sampleData}
+                <GroupProfileView 
+                    groups={getGroups}
                     detailGroup_open={getDetailGroup_open}
                     onLogInUser={loginAccount}
+                    detailGroup_memberLength={detailGroup_memberLength}
                 />
             </div>
         );
