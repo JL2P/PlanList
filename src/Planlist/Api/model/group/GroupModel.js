64b5c1f8c7abf7266f8e21af85597c9912eec5dd
2myungho <dl_audgh@naver.com>
2020-11-05 18:51:07 +0900

    프록시로 감싸 있는 member를 꺼내는 작업

--- src/Planlist/Api/model/group/GroupModel.js
+++ src/Planlist/Api/model/group/GroupModel.js
@@ -1,3 +1,5 @@
+import MemberModel from '../member/MemberModel';
+
 export default class GroupModel{
     constructor(groupObj){
         this.id = groupObj.id;
@@ -6,5 +8,6 @@ export default class GroupModel{
         this.description = groupObj.description //설명글
         this.category = groupObj.category; //카테고리
         this.master = groupObj.master; //그룹장
+        this.members = groupObj.members.map(member=>new MemberModel(member));
     }
 }
