260fafc2cbbacdfcff0ea918e757842058a0c842
2myungho <dl_audgh@naver.com>
2020-11-09 22:20:17 +0900

    그룹 가입하기 버튼 클릭시 member 테이블에 데이터 저장

--- src/Planlist/Group/GroupDetail/Container/DetailProfileContainer.jsx
+++ src/Planlist/Group/GroupDetail/Container/DetailProfileContainer.jsx
@@ -5,6 +5,15 @@ import { inject, observer } from "mobx-react";
 @inject("Store")
 @observer
 class DetailProfileContainer extends Component {
+
+    onGroupJoin = (e,memberObj) => {
+        console.log(memberObj)
+        const { group } = this.props.Store;
+        group.confirm = false;
+        group.manager = false;
+        group.groupMember(memberObj)
+    }
+
     render() {
         const { group } = this.props.Store;
         const { account } = this.props.Store;
@@ -12,7 +21,10 @@ class DetailProfileContainer extends Component {
         const {
             getDetailGroup_open,
             detailGroup_memberLength,
-            getGroups
+            getGroups,
+            getDetailGroup_memberList,
+            getConfirm,
+            getManager
         } = group
 
         const {loginAccount} = account;
@@ -24,6 +36,10 @@ class DetailProfileContainer extends Component {
                     detailGroup_open={getDetailGroup_open}
                     onLogInUser={loginAccount}
                     detailGroup_memberLength={detailGroup_memberLength}
+                    detailGroup_memberList ={getDetailGroup_memberList}
+                    onGroupJoin={this.onGroupJoin}
+                    memberConfirm={getConfirm}
+                    memberManager={getManager}
                 />
             </div>
         );
