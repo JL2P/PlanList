ee821a498e827c1d4518143dfa03a0c230cafe55
2myungho <dl_audgh@naver.com>
2020-11-10 22:31:59 +0900

    인증을 위한 HEADER 추가

--- src/Planlist/Api/Repository/GroupRepository.js
+++ src/Planlist/Api/Repository/GroupRepository.js
@@ -1,5 +1,11 @@
 import axios from "axios"
 
+const HEADER = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
+    },
+  };
+
 //Account관련 Api와 연동하는 클래스
 export default class GroupRepository{
     //공통 적으로 사용되는 URL
@@ -8,30 +14,30 @@ export default class GroupRepository{
     //group list 조회
     // GET / api/group/
     groupList = () => {
-        return axios.get(this.URL).then(request => request.data||[])
+        return axios.get(this.URL,HEADER).then(request => request.data||[])
     }
     //group 조회
     // GET /api/group/{groupId}/
     groupDetail = (groupId) =>{
-        return axios.get(this.URL+`${groupId}/`).then(request=>request.data||{})
+        return axios.get(this.URL+`${groupId}/`,HEADER).then(request=>request.data||{})
     }
 
     //group 추가
     //POST /api/group/
     groupCreate = (GroupModel) => {
-        return axios.post(this.URL,GroupModel).then(request => request.data||[])
+        return axios.post(this.URL,GroupModel,HEADER).then(request => request.data||[])
     }
     
     // group 수정
     // PUT /api/group/
     groupModify = (GroupModel)=>{
-        return axios.put(this.URL,GroupModel).then(request=>request.data||{})
+        return axios.put(this.URL,GroupModel,HEADER).then(request=>request.data||{})
     }
 
     //group 삭제
     // DELETE /api/group/{groupId}
     groupDelete = (groupId) => {
-        return axios.delete(this.URL+`${groupId}/`).then(request=>request.data||null)
+        return axios.delete(this.URL+`${groupId}/`,HEADER).then(request=>request.data||null)
     }    
 
 }
