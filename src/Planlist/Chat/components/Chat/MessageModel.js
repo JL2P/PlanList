79dc8e3e52d61dd5967c034a9195fe9a776e0004
shoon2430 <shoon2430@gmail.com>
2020-11-19 09:31:04 +0900

    채팅 테스트

---
+++ src/Planlist/Chat/components/Chat/MessageModel.js
@@ -0,0 +1,12 @@
+
+class MessageModel{
+    constructor(messageObj){
+        this.messageId = messageObj.id;
+        this.type = messageObj.type || 'TALK';
+        this.username = messageObj.username;
+        this.content = messageObj.content;
+        this.data = messageObj.data;
+    }
+}
+
+export default MessageModel
