b1cdec04dce85dca9dd2a5004e431eabff5baec8
song <eunsonglee0204@gmail.com>
2020-11-20 13:01:54 +0900

    calendar 라이브러리 추가

--- src/Planlist/Account/Profile/view/CalendarItem/ProfileCalendarView.jsx
+++ src/Planlist/Account/Profile/view/CalendarItem/ProfileCalendarView.jsx
@@ -1,9 +1,43 @@
 import React from "react";
-
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+// import Calendar from "react-full-event-calendar";
+import events from "./events";
 const ProfileCalendarView = ({ selectUser, todos }) => {
-  console.log("하이", selectUser.accountId);
+  // const events = [{ title: "today's event", date: new Date() }];
 
-  return <div>Calendar</div>;
+//   const events = [
+//     {
+//       startTime: new Date(),
+//       endTime: new Date(2020, 11, 30),
+//       title: "working in the weekend",
+//     },
+//     {
+//       startTime: new Date(moment().add(-3, "hour")),
+//       endTime: new Date(moment().add(-2, "hour")),
+//       title: "working in the weekend",
+//     },
+//     {
+//       startTime: new Date(),
+//       endTime: new Date(moment().add(1, "hour")),
+//       title: "working in the weekend",
+//     },
+//   ];
+
+  console.log("하이", selectUser.accountId);
+  console.log(new Date());
+  console.log(new Date(2020, 11, 30));
+  //   console.log(new Date("Thu Nov 19 2020 18:25:04 GMT+0900").getFullYear() + 1);
+  //   console.log(new Date("Thu Nov 19 2020 18:25:04 GMT+0900").getMonth() + 1);
+  return (
+    <FullCalendar
+      defaultView="dayGridMonth"
+      plugins={[dayGridPlugin]}
+      events={events}
+    />
+
+    // <Calendar events={events} />
+  );
 };
 
 export default ProfileCalendarView;
