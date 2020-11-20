789fd22436f32d1ebe0013550208bd20d08b22f1
song <eunsonglee0204@gmail.com>
2020-11-20 14:38:37 +0900

    달력에 todo 데이터 가져와서 뿌리기

--- src/Planlist/Account/Profile/view/CalendarItem/ProfileCalendarView.jsx
+++ src/Planlist/Account/Profile/view/CalendarItem/ProfileCalendarView.jsx
@@ -1,34 +1,27 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-// import Calendar from "react-full-event-calendar";
-import events from "./events";
+
 const ProfileCalendarView = ({ selectUser, todos }) => {
-  // const events = [{ title: "today's event", date: new Date() }];
+  const events = [];
+  todos.map((item) => {
+    const end = new Date(item.endTime);
+    end.setDate(end.getDate() + 1);
+    const endTime =
+      end.getFullYear() +
+      "-" +
+      (end.getMonth() + 1).toString().padStart(2, "0") +
+      "-" +
+      end.getDate().toString().padStart(2, "0");
+    events.push({
+      title: item.title,
+      start: item.created.substring(0, 10),
+      end: endTime,
+      color: item.completed === "Y" ? "#CED4DA" : "#FFB517",
+      url: "http://google.com/",
+    });
+  });
 
-//   const events = [
-//     {
-//       startTime: new Date(),
-//       endTime: new Date(2020, 11, 30),
-//       title: "working in the weekend",
-//     },
-//     {
-//       startTime: new Date(moment().add(-3, "hour")),
-//       endTime: new Date(moment().add(-2, "hour")),
-//       title: "working in the weekend",
-//     },
-//     {
-//       startTime: new Date(),
-//       endTime: new Date(moment().add(1, "hour")),
-//       title: "working in the weekend",
-//     },
-//   ];
-
-  console.log("하이", selectUser.accountId);
-  console.log(new Date());
-  console.log(new Date(2020, 11, 30));
-  //   console.log(new Date("Thu Nov 19 2020 18:25:04 GMT+0900").getFullYear() + 1);
-  //   console.log(new Date("Thu Nov 19 2020 18:25:04 GMT+0900").getMonth() + 1);
   return (
     <FullCalendar
       defaultView="dayGridMonth"
