6ae40b6a12991ab45e69efb0c3f840c9705dab57
2myungho <dl_audgh@naver.com>
2020-11-24 16:00:02 +0900

    groupGallery 추가 기능

---
+++ src/Planlist/Api/Repository/GroupGalleryRepository.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+const HEADER = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
+    },
+  };
+
+//member관련 Api와 연동하는 클래스
+export default class MemberRepository{
+    //공통 적으로 사용되는 URL
+    URL = "/api/groups/";
+
+
+    //member 추가
+    //POST /api/groups/
+    galleryAdd = (filePath) => {
+        let formData = new FormData()
+        formData.append("file", filePath)
+        return axios.post(this.URL+"gallery", formData, HEADER).then(request => request.data||[])
+    }
+    
+}
