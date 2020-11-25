f39a2f545f439cd59fee04ad2fb6280b5fc67fc0
shoon2430 <shoon2430@gmail.com>
2020-11-26 04:12:09 +0900

    메인화면 에니메이션 추가

--- src/Planlist/Main/View/MainCreateTodoDesktopView.jsx
+++ src/Planlist/Main/View/MainCreateTodoDesktopView.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Header } from "semantic-ui-react";
-
+import MainBackgroundView from "./MainBackgroundView";
+import "./Main.css";
 const MainCreateTodoDesktopView = ({
   title,
   onChangeTitle,
@@ -8,37 +9,43 @@ const MainCreateTodoDesktopView = ({
   onEnterCreateTodoModal,
 }) => {
   return (
-    <Container
-      text
-      textAlign="center"
-      style={{ paddingTop: "1em", paddingBottom: "3.5em" }}
-    >
-      <Header
-        as="h5"
-        content="어떤 계획을 생각하고 계신가요?"
+    <Container fluid textAlign="center" className="MainBox">
+      <div className="SubMainBox"></div>
+      <MainBackgroundView
         style={{
-          fontSize: "1.5em",
-          fontWeight: "normal",
-          color: "white",
+          paddingTop: "1em",
+          paddingBottom: "3.5em",
         }}
       />
 
-      <div className="ui action input" style={{ width: "90%" }}>
-        <input
-          type="text"
-          placeholder="오늘 할일을 적어보세요"
-          value={title}
-          onChange={onChangeTitle}
-          onKeyPress={onEnterCreateTodoModal}
-        />
-        <button
-          className="ui icon inverted  button"
-          onClick={() => {
-            onCreateTodoModal(true);
+      <div className="SubMainBox2">
+        <Header
+          as="h5"
+          content="어떤 계획을 생각하고 계신가요?"
+          style={{
+            fontSize: "1.5em",
+            fontWeight: "normal",
+            color: "white",
           }}
-        >
-          <i aria-hidden="true" className="write icon"></i>
-        </button>
+        />
+
+        <div className="ui action input MainInput">
+          <input
+            type="text"
+            placeholder="오늘 할일을 적어보세요"
+            value={title}
+            onChange={onChangeTitle}
+            onKeyPress={onEnterCreateTodoModal}
+          />
+          <button
+            className="ui icon inverted  button"
+            onClick={() => {
+              onCreateTodoModal(true);
+            }}
+          >
+            <i aria-hidden="true" className="write icon"></i>
+          </button>
+        </div>
       </div>
     </Container>
   );
