7bd6113ab5b4531fc8ab25fc41d7b5695e30311d
2myungho <dl_audgh@naver.com>
2020-11-26 03:02:26 +0900

    Account Gallery 연동

---
+++ src/Planlist/Api/Repository/AccountGalleryRepository.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+const HEADER = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
+    },
+  };
+
+//member관련 Api와 연동하는 클래스
+export default class AccountGalleryRepository{
+    //공통 적으로 사용되는 URL
+    URL = "/api/accounts/";
+
+
+    //POST /api/account/
+    galleryAdd = (filePath, accountId) => {
+        let formData = new FormData()
+        formData.append("file", filePath)
+        return axios.post(this.URL+`${accountId}/gallery`, formData, HEADER).then(request => request.data||[])
+    }
+    
+}
