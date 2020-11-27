ec8b6ac25e7e6d6c6cf2c925afa5307a3dc6d1f9
shoon2430 <shoon2430@gmail.com>
2020-11-27 09:38:06 +0900

    팔로우 팔로윙 모달 닫기 버튼 추가

--- src/Planlist/Account/Profile/container/ProfileFollowingListContainer.jsx
+++ src/Planlist/Account/Profile/container/ProfileFollowingListContainer.jsx
@@ -1,41 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
-import {Header, Item} from 'semantic-ui-react'
+import { Header, Item } from "semantic-ui-react";
 import ProfileFollowingView from "../view/ProfileManageFollowing/ProfileFollowingView";
-
 
 @inject("Store")
 @observer
 class ProfileFollowingListContainer extends Component {
+  onDeleteMyFollowing = (followId) => {
+    alert("삭제되었습니다");
+    const { follow } = this.props.Store;
+    follow.deleteMyFollowing(followId);
+    window.location.reload();
+  };
 
-    onDeleteMyFollowing = (followId) => {
-        alert("삭제되었습니다");
-        const { follow } = this.props.Store;
-        follow.deleteMyFollowing(followId);
-        window.location.reload();
-    } 
-
-    render(){
-        const { follow } = this.props.Store;
-        const myFollowings = follow.getMyFollowings;
-        const element = myFollowings.map((following) => <ProfileFollowingView key={following.accountId} 
+  render() {
+    const { follow } = this.props.Store;
+    const myFollowings = follow.getMyFollowings;
+    const element = myFollowings.map((following) => (
+      <ProfileFollowingView
+        key={following.accountId}
         following={following}
         onDeleteMyFollowing={this.onDeleteMyFollowing}
-        />);   
+      />
+    ));
 
-        return (
-            <div style={{height:"600px", textAlign:"center"}}>
-                <Header as="h3" dividing style={{marginTop:"0.5em"}}>팔로잉</Header>
-                <div>
-                <Item.Group>
-                    {element}
-                </Item.Group>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div style={{ height: "600px", textAlign: "center" }}>
+        <div>
+          <Item.Group>{element}</Item.Group>
+        </div>
+      </div>
+    );
+  }
 }
-
-
 
 export default ProfileFollowingListContainer;
--- src/Planlist/Account/Profile/view/ProfileManageFollower/ProfileFollowerModalView.jsx
+++ src/Planlist/Account/Profile/view/ProfileManageFollower/ProfileFollowerModalView.jsx
@@ -1,62 +1,34 @@
-import React from 'react';
-import { Modal, Header, Icon, Divider} from 'semantic-ui-react'
-import ProfileFollowerListContainer from "../../container/ProfileFollowerListContainer"
+import React from "react";
+import { Modal, Header } from "semantic-ui-react";
+import ProfileFollowerListContainer from "../../container/ProfileFollowerListContainer";
 
-const ProfileFollowerModalView = ({
-  followerOpen,
-  onFollowerModal
- 
-}) => {
+const ProfileFollowerModalView = ({ followerOpen, onFollowerModal }) => {
   return (
-    
-    <Modal 
+    <Modal
+      closeIcon
       onClose={() => onFollowerModal(false)}
       onOpen={() => onFollowerModal(true)}
       open={followerOpen}
       size="tiny"
-      >
-          <div style={{ margin: "1em" }}>
-          <div
-            style={{
-              display: "flex",
-              
-              alignItems: "center",
-            }}
-        >
-
-              
-      
-        <Header as="h3" dividing style={{
-          marginTop:"0.5em" ,
-          display: "flex",
-          justifyContent: "center",
-          
-          alignItems: "center",
+    >
+      <div style={{ margin: "1em" }}>
+        <Header
+          as="h3"
+          dividing
+          style={{
+            marginTop: "0.5em",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-          content="팔로워" />
-         
-        <Icon
-              name="x"
-              size="big"
-              style={{ 
-                marginBottom: "0.5em",
-                display: "flex",
-                justifyContent: "end",
-                alignItems: "right",
-                
-              }}
-              onClick={() => {
-                onFollowerModal(false);
-              }}
-            /> 
-            </div>
-          
-        
+          content="팔로워"
+        />
+
         <Modal.Content scrolling style={{ width: "100%" }}>
-        <ProfileFollowerListContainer />
-      </Modal.Content>
-   </div>
-  </Modal>
-  )
-}
+          <ProfileFollowerListContainer />
+        </Modal.Content>
+      </div>
+    </Modal>
+  );
+};
 export default ProfileFollowerModalView;
