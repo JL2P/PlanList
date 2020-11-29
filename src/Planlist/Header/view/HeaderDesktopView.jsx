6ef151156387a0d332c9d406f79b97f5f3028602
shoon2430 <shoon2430@gmail.com>
2020-11-30 00:59:09 +0900

    구글로그인 및 에러 수정

--- src/Planlist/Header/view/HeaderDesktopView.jsx
+++ src/Planlist/Header/view/HeaderDesktopView.jsx
@@ -1,49 +1,47 @@
 import React, { useState } from "react";
 import { Menu, Visibility } from "semantic-ui-react";
 import HeaderIconsView from "./HeaderIconsView";
-import { Link } from "react-router-dom";
 
 const menuFixed = { background: "rgb(27, 28, 29)" };
 
-const HeaderDesktopView = ({ Media, loginAccount, loginCheck, accounts, notConfirmFollowers,element }) => {
+const HeaderDesktopView = ({
+  loginAccount,
+  loginCheck,
+  accounts,
+  notConfirmFollowers,
+  element,
+}) => {
   const [fixed, setFixed] = useState(false);
 
   const hideFixedMenu = () => setFixed(false);
   const showFixedMenu = () => setFixed(true);
 
   return (
-    <Media greaterThan="mobile">
-      <Visibility
-        once={false}
-        onBottomPassed={showFixedMenu}
-        onBottomPassedReverse={hideFixedMenu}
+    <Visibility
+      once={false}
+      onBottomPassed={showFixedMenu}
+      onBottomPassedReverse={hideFixedMenu}
+    >
+      <Menu
+        fixed={fixed ? "top" : undefined}
+        inverted
+        pointing
+        secondary
+        borderless
+        style={menuFixed ? menuFixed : menuFixed}
+        size="tiny"
       >
-        <Menu
-          fixed={fixed ? "top" : undefined}
-          inverted
-          pointing
-          secondary
-          borderless
-          style={menuFixed ? menuFixed : menuFixed}
-          size="tiny"
-        >
-          <HeaderIconsView 
-            accounts={accounts} 
-            loginAccount={loginAccount}
-             notConfirmFollowers={notConfirmFollowers} 
-             element={element} 
-          />
-          {loginCheck && (
-            <h2 style={{ color: "white" }}>{loginAccount.accountId}</h2>
-          )}
-          {!loginCheck && (
-            <Link to="/signin">
-              <h1>로그인하기</h1>
-            </Link>
-          )}
-        </Menu>
-      </Visibility>
-    </Media>
+        <HeaderIconsView
+          accounts={accounts}
+          loginAccount={loginAccount}
+          notConfirmFollowers={notConfirmFollowers}
+          element={element}
+        />
+        {loginCheck && (
+          <h2 style={{ color: "white" }}>{loginAccount.accountId}</h2>
+        )}
+      </Menu>
+    </Visibility>
   );
 };
 
