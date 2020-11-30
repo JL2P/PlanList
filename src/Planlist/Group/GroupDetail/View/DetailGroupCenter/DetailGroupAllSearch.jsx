172054308da69b414ef1353481cd49143df39815
2myungho <dl_audgh@naver.com>
2020-11-30 22:46:35 +0900

    그룹 내 그룹 투두 검색 기능

---
+++ src/Planlist/Group/GroupDetail/View/DetailGroupCenter/DetailGroupAllSearch.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import _ from "lodash";
+import { Search } from "semantic-ui-react";
+import "../../../GroupStyle/Search.scss";
+
+var searchList = [];
+
+const DetailGroupAllSearch = ({ groupTodos }) => {
+    searchList = groupTodos;
+
+    const initialState = {
+        loading: false,
+        results: [],
+        value: "",
+    };
+
+    function exampleReducer(state, action) {
+        switch (action.type) {
+            case "CLEAN_QUERY":
+                return initialState;
+            case "START_SEARCH":
+                return { ...state, loading: true, value: action.query };
+            case "FINISH_SEARCH":
+                return { ...state, loading: false, results: action.results };
+            case "UPDATE_SELECTION":
+                return { ...state, value: action.selection };
+
+            default:
+                throw new Error();
+        }
+    }
+
+    function source() {
+        let test = searchList.map((val) => {
+            return {
+                title: val.title,
+                //   description: val.description,
+                image: val.imgUrl,
+                id: val.groupTodoId,
+            };
+        });
+
+        return test;
+    }
+
+    const [state, dispatch] = React.useReducer(exampleReducer, initialState);
+    const { loading, results, value } = state;
+
+    const timeoutRef = React.useRef();
+    const handleSearchChange = React.useCallback((e, data) => {
+        clearTimeout(timeoutRef.current);
+        dispatch({ type: "START_SEARCH", query: data.value });
+
+        timeoutRef.current = setTimeout(() => {
+            if (data.value.length === 0) {
+                dispatch({ type: "CLEAN_QUERY" });
+                return;
+            }
+
+            const re = new RegExp(_.escapeRegExp(data.value), "i");
+            dispatch({
+                type: "FINISH_SEARCH",
+                results: source()
+                    .slice()
+                    .filter((val) => {
+                        return re.test(val.title);
+                    }),
+            });
+        }, 300);
+    }, []);
+
+    React.useEffect(() => {
+        return () => {
+            clearTimeout(timeoutRef.current);
+        };
+    }, []);
+
+    // //검색 후 이동
+    // const onMove = (groupId) => {
+    //     window.location.replace(`/groupDetail/${groupId}`);
+    // };
+
+    return (
+        <Search
+            style={{ height: "3.2em" }}
+            loading={loading}
+            onResultSelect={(e, data) => {
+                // onMove(data.result.id);
+                dispatch({ type: "UPDATE_SELECTION", selection: data.result.title });
+            }}
+            onSearchChange={handleSearchChange}
+            results={results}
+            value={value}
+            className="GroupTodoSearch"
+            placeholder="게시물을 입력해주세요."
+        />
+    );
+};
+
+export default DetailGroupAllSearch;
--- src/Planlist/Group/GroupDetail/View/DetailGroupCenter/DetailGroupPrivate.jsx
+++ src/Planlist/Group/GroupDetail/View/DetailGroupCenter/DetailGroupPrivate.jsx
@@ -3,8 +3,13 @@ import { Button, Container, Header, Icon, Segment } from "semantic-ui-react";
 
 const DetailGroupPrivate = () => {
     return (
-        <Segment textAlign="center">
-          <Header style={{ marginTop: "15px" }}>비공개 그룹입니다</Header>
+        <Segment textAlign="center" style={{
+            height:"400px",
+            display:"flex",
+            justifyContent:"center",
+            flexDirection:"column"
+        }}>
+          <Header style={{ marginTop: "0" }}>비공개 그룹입니다</Header>
             그룹에 가입해서 다른 사람들과 함께 계획을 실천해 보세요!
             <Segment.Inline style={{ marginTop: "15px" }}>
              
