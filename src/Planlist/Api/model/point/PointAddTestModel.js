991358016b391b7b07e7d80ee6212f26680b7b24
shoon2430 <shoon2430@gmail.com>
2020-12-03 14:52:12 +0900

    그룹계획 오류 수정

---
+++ src/Planlist/Api/model/point/PointAddTestModel.js
@@ -0,0 +1,9 @@
+export default class PointAddTestModel {
+    constructor(pointObj) {
+      this.accountId = pointObj.accountId;
+      this.todoId = pointObj.todoId;
+      this.likeCount = pointObj.likeCount;
+      this.created = pointObj.created;
+    }
+  }
+  
